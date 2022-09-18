1eaf1b8db3044a39a393a5be64b392360e922fca
Simo Aleksandrov <simo3003@me.com>
2022-09-18 19:34:09 +0300

    feat: make bootstrap.sh import agekey automatically

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+kubectl create namespace flux-system
+
+export SOPS_AGE_KEY_FILE=$(cat .sops.yaml | yq -r '.creation_rules[0].age')
+
+cat age.agekey |
+	kubectl create secret generic sops-age \
+	--namespace=flux-system \
+	--from-file=sops.agekey=/dev/stdin
+
 flux bootstrap github \
   --owner=fr3fou \
   --repository=home \
