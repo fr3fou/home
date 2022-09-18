1eaf76559387d16e3711b47e61e22a1b7a3e7289
Simo Aleksandrov <simo3003@me.com>
2022-09-18 18:29:53 +0300

    chore: make install script bind to first IP

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--disable helm-controller --disable traefik" sh -
-echo "Finished installing k3s..."
-echo "kubeconfig:"
-sudo cat /etc/rancher/k3s/k3s.yaml
+
+IP=$(hostname -I | awk '{print $1}')
+
+curl -sfL https://get.k3s.io | \
+	INSTALL_K3S_EXEC="--disable helm-controller \
+		--disable traefik \
+		--bind-address ${IP} \
+		--node-ip ${IP} \
+		--advertise-address ${IP} \
+		--node-external-ip ${IP}" \
+	sh -  >/dev/null
+
+until [ -f /etc/rancher/k3s/k3s.yaml ]
+do
+	sleep 1
+done
+
+cat /etc/rancher/k3s/k3s.yaml
